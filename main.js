0aa6b98ba8543d548994d29ddccb295ae0b6fdff
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-07-15 11:12:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }    
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');            
+    }
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
-/*
-let  contador = 0;
 
-//----------enquanto-----------
-
-while (contador < listaDeTeclas.length) {
-
-    const tecla = listaDeTeclas[contador];
-    const  instrumento = tecla.classList[1];
-
-     //template string
-    const idAudio = `#som_${instrumento}`;
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-    contador =contador + 1;
-}
-*/
 //-----------para---------
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
@@ -47,3 +36,25 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
 }
+
+
+/*
+let  contador = 0;
+
+//----------enquanto-----------
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const  instrumento = tecla.classList[1];
+
+     //template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador =contador + 1;
+}
+*/
